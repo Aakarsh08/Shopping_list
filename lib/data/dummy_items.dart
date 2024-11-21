fba3997af891f457e4ca605f193d4ffbd0afc971
Aakarsh_08 <gupta08aakarsh@gmail.com>
2024-11-22 01:57:02 +0530

    hb

--- lib/data/dummy_items.dart
+++ lib/data/dummy_items.dart
@@ -1,11 +1,14 @@
+import 'package:shopping_list/models/category.dart';
 import 'package:shopping_list/models/grocery_item.dart';
 import 'package:shopping_list/data/categories.dart';
+
 
 final groceryItems = [
   GroceryItem(
       id: 'a',
       name: 'Milk',
       quantity: 1,
+      
       category: categories[Categories.dairy]!),
   GroceryItem(
       id: 'b',

1bd5530c3a7f72009cb1963a263fbad38816583e
Aakarsh_08 <gupta08aakarsh@gmail.com>
2024-11-28 02:56:35 +0530

    sdgvfs

--- lib/widgets/grocery_list.dart
+++ lib/widgets/grocery_list.dart
@@ -4,7 +4,7 @@ import 'package:shopping_list/models/grocery_item.dart';
 import 'package:shopping_list/widgets/new_item.dart';
 
 class GroceryList extends StatefulWidget {
-  const GroceryList({ super.key });
+  const GroceryList({super.key});
 
   @override
   State<GroceryList> createState() => _GroceryListState();

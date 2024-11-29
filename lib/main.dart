6ca707e3e873a7b1077d6bda8b6abacab9925ada
Aakarsh_08 <gupta08aakarsh@gmail.com>
2024-11-30 03:51:59 +0530

    sfds

--- lib/main.dart
+++ lib/main.dart
@@ -5,7 +5,7 @@ void main() {
   runApp(const MyApp());
 }
 
-class MyApp extends  StatelessWidget {
+class MyApp extends StatelessWidget {
   const MyApp( {super.key} );
 
   // This widget is the root of your application.
@@ -18,7 +18,7 @@ class MyApp extends  StatelessWidget {
         colorScheme: ColorScheme.fromSeed(
           seedColor: const Color.fromARGB(255, 147, 229, 250),
           brightness: Brightness.dark,
-          surface: const Color.fromARGB(255, 42, 51, 59),
+          surface:  const Color.fromARGB(255, 42, 51, 59),
         ),
         scaffoldBackgroundColor: const Color.fromARGB(255, 50, 58, 60),
       ),

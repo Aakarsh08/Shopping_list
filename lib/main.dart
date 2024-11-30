9bffcf778edff8378bec64fed05c0663d88a64bf
Aakarsh_08 <gupta08aakarsh@gmail.com>
2024-12-01 02:05:25 +0530

    dfgsd

--- lib/main.dart
+++ lib/main.dart
@@ -15,12 +15,12 @@ class MyApp extends StatelessWidget {
       title: 'Flutter Groceries',
       theme: ThemeData.dark().copyWith(
         useMaterial3: true,
-        colorScheme: ColorScheme.fromSeed (
+        colorScheme: ColorScheme.fromSeed(
           seedColor: const Color.fromARGB(255, 147, 229, 250),
           brightness: Brightness.dark,
           surface:  const Color.fromARGB(255, 42, 51, 59),
         ),
-        scaffoldBackgroundColor: const Color.fromARGB(255, 50, 58, 60),
+        scaffoldBackgroundColor : const Color.fromARGB(255, 50, 58, 60),
       ),
       home: const GroceryList(),
     );

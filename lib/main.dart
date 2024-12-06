8dc6f49b7f8193ca964631c0704e658220ae5205
Aakarsh_08 <gupta08aakarsh@gmail.com>
2024-12-06 19:52:40 +0530

    mbnv

--- lib/main.dart
+++ lib/main.dart
@@ -18,7 +18,7 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(
           seedColor: const Color.fromARGB(255, 147, 229, 250),
           brightness: Brightness.dark,
-          surface:  const Color.fromARGB(255, 42, 51, 59),
+          surface: const Color.fromARGB(255, 42, 51, 59),
         ),
         scaffoldBackgroundColor: const Color.fromARGB(255, 50, 58, 60),
       ),
